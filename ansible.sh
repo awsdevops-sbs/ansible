388189fa267d4a2a970b6a297359037305b51798
akram <akram.khan200@gmail.com>
2026-01-02 14:35:02 +0530

    gocd pipelines

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 #export ANSIBLE_HOST_KEY_CHECKING=False
 component=$1
 
-env=$2
+#env=$2
 #ansible-playbook  -i "$component-dev.awsdevops.sbs," -e ansible_username=ec2-user -e ansible_password=DevOps321  -e role_name="$component" -e env=dev expense.yml
 
 
