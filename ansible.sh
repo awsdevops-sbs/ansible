259f91a88f2abc7ebd48e2a4a7f5034feada7f8f
akram <akram.khan200@gmail.com>
2026-01-08 21:34:53 +0530

    tools set up 1.1.2

--- ansible.sh
+++ ansible.sh
@@ -5,10 +5,10 @@ component=$1
 #ansible-playbook  -i "$component-dev.awsdevops.sbs," -e ansible_username=ec2-user -e ansible_password=DevOps321  -e role_name="$component" -e env=dev expense.yml
 
 
-ansible-playbook -i "$component-$env.awsdevops.sbs," \
-  -e ansible_user=ec2-user \
-  -e ansible_password=DevOps321 \
-  -e role_name="$component" \
-  -e env=$env \
-  -e mysql_password=ExpenseApp@1 \
-  expense.yml
+#ansible-playbook -i "$component-$env.awsdevops.sbs," \
+#  -e ansible_user=ec2-user \
+#  -e ansible_password=DevOps321 \
+#  -e role_name="$component" \
+#  -e env=$env \
+#  -e mysql_password=ExpenseApp@1 \
+#  expense.yml
