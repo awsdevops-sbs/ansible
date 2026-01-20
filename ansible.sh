561c70c1dd6aca61eac2eecadb658ff5ffe8efe3
akram <akram.khan200@gmail.com>
2026-01-20 17:07:25 +0530

    install Vault

--- ansible.sh
+++ ansible.sh
@@ -5,10 +5,24 @@ component=$1
 #ansible-playbook  -i "$component-dev.awsdevops.sbs," -e ansible_username=ec2-user -e ansible_password=DevOps321  -e role_name="$component" -e env=dev expense.yml
 
 
+#ansible-playbook -i "$component-$env.awsdevops.sbs," \
+#  -e ansible_user=ec2-user \
+#  -e ansible_password=DevOps321 \
+#  -e role_name="$component" \
+#  -e env=$env \
+#  -e mysql_password=ExpenseApp@1 \
+#  expense.yml
+
+
+ansible-playbook get-secrets.yml  \
+-e role_name="$component" \
+  -e env=$env \
+  -e vault_token=$vault_token \
+   -e '@secret.json'
+
 ansible-playbook -i "$component-$env.awsdevops.sbs," \
-  -e ansible_user=ec2-user \
-  -e ansible_password=DevOps321 \
   -e role_name="$component" \
   -e env=$env \
   -e mysql_password=ExpenseApp@1 \
-  expense.yml
+  expense.yml  \
+   -e '@secret.json'
