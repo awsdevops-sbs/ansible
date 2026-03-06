f100a3ab6205f986568cc3c4a1e0c6628e16b22b
akram <akram.khan200@gmail.com>
2026-03-06 22:46:34 +0530

    load balacer

--- ansible.sh
+++ ansible.sh
@@ -28,15 +28,9 @@ ansible-playbook get-secrets.yml  \
   -e vault_token=$vault_token
 
 
-  #ansible-playbook -i $component-$env.awsdevops16297.sbs,  -e env=$env -e role_name=$component expense.yml -e '@~/secret.json' -e '@~/app.json' -e '@~/newrelic_key.json'
+  ansible-playbook -i $component-$env.awsdevops16297.sbs,  -e env=$env -e role_name=$component expense.yml -e '@~/secret.json' -e '@~/app.json' -e '@~/newrelic_key.json'
 
-ansible-playbook -i "$component-$env.awsdevops16297.sbs," \
-expense.yml \
--e env=$env \
--e role_name=$component \
--e "@~/secret.json" \
--e "@~/app.json" \
--e "@~/newrelic_key.json"
+
 
 
 #ansible-playbook -i "$component-$env.awsdevops16297.sbs," \
